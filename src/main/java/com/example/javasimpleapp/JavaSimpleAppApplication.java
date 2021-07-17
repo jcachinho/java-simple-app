1366897ed41fb165b07d670a45098f2cf56ed0b2
João Cachinho <jcachinho.94@gmail.com>
2021-07-17 01:19:48 +0100

    [JCACH] Added JFrame window

--- src/main/java/com/example/javasimpleapp/JavaSimpleAppApplication.java
+++ src/main/java/com/example/javasimpleapp/JavaSimpleAppApplication.java
@@ -1,13 +1,36 @@
 package com.example.javasimpleapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
-public class JavaSimpleAppApplication {
+import javax.swing.*;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class JavaSimpleAppApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaSimpleAppApplication.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(JavaSimpleAppApplication.class);
+        builder.headless(false);
+        ConfigurableApplicationContext context = builder.run(args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        JFrame f = new JFrame("Test swing app");
+        JButton b=new JButton("click");//creating instance of JButton
+        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+
+        f.add(b);//adding button in JFrame
+
+        f.setSize(400,500);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
+
+
+    }
 }
